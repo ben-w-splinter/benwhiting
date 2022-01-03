996f98ea550f918c5311f3e25be652f2d61df5db
Ben Whiting <beatboxbeastz80@gmail.com>
2022-01-03 17:21:49 +0000

    Added basic template

---
+++ benwhiting/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <h1>Ben Whiting</h1>
+            <h2>Software Developer</h2>
+        </StyledHeader>
+    ) 
+}
---
+++ benwhiting/src/components/styles/Header.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const StyledHeader = styled.header`
+    text-align: center;
+    margin-top: 20px;
+    background-color: grey;
+
+    h1{
+        font-size: 2rem;
+        font-style:italic;
+    }
+`
